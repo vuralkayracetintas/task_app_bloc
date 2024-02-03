e6472268d72170ba1eb8007d6081b07e84ced6a8
vuralkayracetintas <vuralkayracetintass@gmail.com>
2024-02-04 01:39:05 +0300

    service keys ignore

--- lib/product/service/ads_service.dart
+++ lib/product/service/ads_service.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
 
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:task_app/service_keys.dart';
 
 class AdsService {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return ServiceKeys.bannerUIAndroid;
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-6282105172296354/9014459163';
+      return ServiceKeys.bannerUIAndroid;
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -15,9 +16,9 @@ class AdsService {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712';
+      return ServiceKeys.interstitialUIAndroid;
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return ServiceKeys.interstitialUIios;
     } else {
       throw UnsupportedError('Unsupported platform');
     }
